ceaaa4b8bc0e801bb4c070157db8f72ad7457ade
Jin Kwan Kim <kkole3897@gmail.com>
2023-12-11 11:12:47 +0900

    feat(globals.css): gray color theme variant 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,16 @@ const config: Config = {
   theme: {
     colors: {
       gray: {
+        50: 'var(--gray-50)',
+        100: 'var(--gray-100)',
+        200: 'var(--gray-200)',
+        300: 'var(--gray-300)',
+        400: 'var(--gray-400)',
         500: 'var(--gray-500)',
+        600: 'var(--gray-600)',
+        700: 'var(--gray-700)',
+        800: 'var(--gray-800)',
+        900: 'var(--gray-900)',
       },
       red: {
         500: 'var(--red-500)',
