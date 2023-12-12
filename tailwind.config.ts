d0802a38eecc7ecedb1f2aa0d57f4b1a90379931
Jin Kwan Kim <kkole3897@gmail.com>
2023-12-12 11:00:56 +0900

    feat: tailwind screen config 변경
    tailwind는 기본적으로 mobile first인데 반해 블로그는 desktop first로 설정하는 쪽이 적합하기 때문

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,6 +58,13 @@ const config: Config = {
       mono: 'var(--font-mono)',
       sans: 'var(--font-sans)',
     },
+    screens: {
+      '2xl': { max: '1535px' },
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
+    },
     extend: {
       height: {
         header: 'var(--header-height)',
